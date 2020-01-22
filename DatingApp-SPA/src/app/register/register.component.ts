6b43da1c193cad37877cce065b4f28c156e506e4
MazharJafri <mazharjafri716@gmail.com>
2020-01-21 18:23:32 -0600

    Messenger Functionality

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -21,7 +21,7 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.bsConfig = {
-      containerClass: 'theme-red'
+      containerClass: 'theme-blue'
     },
     this.createRegisterForm();
   }
